29695449bea5e10d092b8f1e42b56273b08862ef
mcguffin <joern@flyingletters.com>
2023-09-11 09:40:42 +0200

    map zoom level to reverse geocode detail level

--- src/js/acf-input-osm/index.js
+++ src/js/acf-input-osm/index.js
@@ -712,11 +712,49 @@
 			)
  		},
 		reverseGeocode:function( model ) {
+			const mapZoomLevel = zoom => {
+				/*
+				Map Nominatim detail levels vs. Plugin Detail levels
+				|                 NOMINATIM                     |  ACF OpenStreetMap Field |
+				|-----------------------------------------------|--------------------------|
+				|  zoom | Detail level (DE) | Detail level (US) |    zoom | Detail level   |
+				|-------|-------------------|-------------------|---------|----------------|
+				|     0 | country           | country           |       0 | country        |
+				|     1 | country           | country           |       1 | country        |
+				|     2 | country           | country           |       2 | country        |
+				|     3 | country           | country           |       3 | country        |
+				|     4 | country           | country           |       4 | country        |
+				|     5 | state             | state             |       5 | state          |
+				|     6 | state             | state             |       6 | state          |
+				|     7 | state             | state             |       8 | county         |
+				|     8 | county            | city              |       8 | county         |
+				|     9 | county            | city              |       9 | county         |
+				|    10 | village           | city              |      10 | village/suburb |
+				|    11 | village           | city              |      11 | village/suburb |
+				|    12 | village           | city              |      12 | village/suburb |
+				|    13 | village           | suburb            |      13 | village/suburb |
+				|    14 | postcode          | neighbourhood     |      16 | village/suburb |
+				|    15 | postcode          | neighbourhood     |      16 | village/suburb |
+				|    16 | road (major)      | road (major)      |      18 | building       |
+				|    17 | road (+minor)     | road (+minor)     |      18 | building       |
+				|    18 | building          | building          |      18 | building       |
+				*/
+				const map = {
+					7:  8, // state => country
+					14: 16, // postcode/neighbourhood => major road
+					15: 16, // postcode/neighbourhood => major road
+					16: 18, // major road => building
+					17: 18, // minor road => building
+				}
+				return map[zoom] ?? zoom
+			}
 			var self = this,
-				latlng = { lat: model.get('lat'), lng: model.get('lng') };
+				latlng = { lat: model.get('lat'), lng: model.get('lng') },
+				zoom = mapZoomLevel( self.map.getZoom() );
+
 			this.geocoder.options.geocoder.reverse(
 				latlng,
-				Math.max( 1, self.map.getZoom() ), // must not be 0, results in -Infinity being sent to geocoder
+				self.map.options.crs.scale( zoom ),
 				/**
 				 *	@param array results
 				 */
