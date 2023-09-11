15a9699ebd193c7bcb8f0ff7e79aacdfe74d2a66
mcguffin <joern@flyingletters.com>
2023-09-11 11:36:15 +0200

    build

--- gulpfile.js
+++ gulpfile.js
@@ -101,6 +101,8 @@ gulp.task('watch:scss',cb => {
 	cb()
 });
 
+gulp.task('build',gulp.series('build:scss','build:js'));
+
 gulp.task('watch', cb => {
 	gulp.watch( config.sass.watchPaths, gulp.parallel('dev:scss','watch:scss'));
 	gulp.watch('./src/js/**/*.js',gulp.parallel('dev:js'));
@@ -109,8 +111,8 @@ gulp.task('watch', cb => {
 gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 
 
-// 
-// 
+//
+//
 // function do_scss( src ) {
 // 	var dir = src.substring( 0, src.lastIndexOf('/') ),
 // 		includePaths = ['src/scss/','node_modules/'];
@@ -118,16 +120,16 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 		.pipe( sourcemaps.init() )
 // 		.pipe( sass( { includePaths } ).on('error', sass.logError) )
 // 		.pipe( autoprefixer({
-// 
+//
 // 		}) )
 // 		.pipe( gulp.dest( './assets/css/' + dir ) )
 //         .pipe( sass( { outputStyle: 'compressed', includePaths } ).on('error', sass.logError) )
 // 		.pipe( rename( { suffix: '.min' } ) )
 //         .pipe( sourcemaps.write() )
 //         .pipe( gulp.dest( './assets/css/' + dir ) );
-// 
+//
 // }
-// 
+//
 // function do_js( src ) {
 // 	var dir = src.substring( 0, src.lastIndexOf('/') );
 // 	return gulp.src( './src/js/' + src + '.js' )
@@ -138,7 +140,7 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 		.pipe( sourcemaps.write() )
 // 		.pipe( gulp.dest( './assets/js/' + dir ) );
 // }
-// 
+//
 // function concat_js( src, dest ) {
 // 	return gulp.src( src )
 // 		.pipe( sourcemaps.init() )
@@ -149,8 +151,8 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 		.pipe( rename( { suffix: '.min' } ) )
 // 		.pipe( gulp.dest( './assets/js/' ) );
 // }
-// 
-// 
+//
+//
 // // fake leaflet
 // L = {
 // 	TileLayer: {
@@ -163,12 +165,12 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 	},
 // 	tileLayer:{}
 // }
-// 
+//
 // // write providers data to ./etc
 // gulp.task('providers', function(cb){
 // 	require('./node_modules/leaflet-providers/leaflet-providers.js');
 // 	let providers = L.TileLayer.Provider.providers;
-// 
+//
 // 	// add overlay property to maps and layers
 // 	const isOverlay = ( name, opts ) => {
 // 		if ( 'string' !== typeof opts && opts.opacity && opts.opacity < 1 ) {
@@ -191,35 +193,35 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 			'HERE(v3?).mapLabels',
 // 			'WaymarkedTrails'
 // 		].join('|');
-// 
+//
 // 		return name.match( overlayPattern ) !== null;
 // 	}
-// 
+//
 // 	// HEREv3 manual upgrade until https://github.com/leaflet-extras/leaflet-providers/pull/343 is released
 // 	// L.TileLayer.Provider.providers.HEREv3 = JSON.parse(JSON.stringify(L.TileLayer.Provider.providers.HERE))
 // 	// L.TileLayer.Provider.providers.HEREv3.url = "https://{s}.{base}.maps.ls.hereapi.com/maptile/2.1/{type}/{mapID}/{variant}/{z}/{x}/{y}/{size}/{format}?apiKey={apiKey}&lg={language}";
 // 	// L.TileLayer.Provider.providers.HEREv3.options.apiKey = "<insert your apiKey here>";
 // 	// delete( L.TileLayer.Provider.providers.HEREv3.options.app_code )
 // 	// delete( L.TileLayer.Provider.providers.HEREv3.options.app_id )
-// 
+//
 // 	 // 52d2aca6-c3b6-4c59-b9de-5df4f4d056bd
-// 
+//
 // 	 delete( L.TileLayer.Provider.providers.USGS ) // Remove, lots of 404s
-// 
+//
 // 	 // missing default variants, will break JS
 // 	 L.TileLayer.Provider.providers.JusticeMap.options.variant = 'income'
 // 	 L.TileLayer.Provider.providers.WaymarkedTrails.options.variant = 'hiking'
 // 	 L.TileLayer.Provider.providers.NASAGIBS.options.variant = 'MODIS_Terra_CorrectedReflectance_TrueColor'
 // 	 L.TileLayer.Provider.providers.nlmaps.options.variant = 'brtachtergrondkaart'
-// 
+//
 // 	 // delete seemingly broken stamen variants
 // 	 delete( L.TileLayer.Provider.providers.Stamen.variants.TopOSMRelief);
 // 	 delete( L.TileLayer.Provider.providers.Stamen.variants.TopOSMFeatures);
-// 
+//
 // 	 // remove HikeBike #83
 // 	 delete( L.TileLayer.Provider.providers.HikeBike );
-// 
-// 
+//
+//
 // /*
 // 	// add MAPBOX ids as variant. See https://www.mapbox.com/api-documentation/#maps
 // 	let mapbox_variants = [
@@ -250,7 +252,7 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 	L.TileLayer.Provider.providers.MapBox.url = L.TileLayer.Provider.providers.MapBox.url.
 // 		replace('{id}','{variant}');
 // 	L.TileLayer.Provider.providers.MapBox.options.variant = 'mapbox.streets';
-// 
+//
 // 	// remove falsy configuration
 // 	delete( L.TileLayer.Provider.providers.MapBox.options.id );
 // 	// END mapbox
@@ -297,19 +299,19 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 	} );
 // 	return fs.writeFile( './etc/leaflet-providers.json', JSON.stringify( providers, null, '\t' ), cb );
 // });
-// 
-// 
-// 
+//
+//
+//
 // gulp.task('scss:edit', function() {
 // 	return do_scss('acf-input-osm');
 // });
-// 
-// 
+//
+//
 // gulp.task('scss:settings', function() {
 // 	return do_scss('acf-osm-settings');
 // });
 // gulp.task('scss', gulp.parallel('scss:edit','scss:settings'));
-// 
+//
 // gulp.task('leaflet-css', gulp.parallel(
 // 		function() {
 // 			// frontend
@@ -342,26 +344,26 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 			])
 // 				.pipe( gulp.dest( './assets/' ) );
 // 		}
-// 
+//
 // 	)
 // );
-// 
-// 
-// 
-// 
+//
+//
+//
+//
 // gulp.task('js-field-group', function() {
 //     return concat_js( [
 // 			'./src/js/acf-field-group-osm.js',
 // 		], 'acf-field-group-osm.js');
 // });
-// 
+//
 // gulp.task('js-admin', function() {
 //     return concat_js( [
 // 			'./src/js/acf-input-osm.js',
 // 		], 'acf-input-osm.js');
 // });
-// 
-// 
+//
+//
 // gulp.task( 'js-frontend', function(){
 // 	return concat_js( [
 // 			'./node_modules/resize-observer-polyfill/dist/ResizeObserver.js',
@@ -370,38 +372,38 @@ gulp.task('dev',gulp.series('dev:scss','dev:js','watch'));
 // 			'./node_modules/leaflet-providers/leaflet-providers.js',
 // 			'./node_modules/leaflet.locatecontrol/src/L.Control.Locate.js',
 // 			'./src/js/acf-osm-frontend.js',
-// 
+//
 // 		], 'acf-osm-frontend.js');
 // } );
-// 
-// 
+//
+//
 // gulp.task('js-settings', function() {
 //     return concat_js( [
 // 			'./src/js/acf-osm-settings.js',
 // 		], 'acf-osm-settings.js');
 // });
-// 
-// 
+//
+//
 // gulp.task('js', gulp.parallel('js-frontend','js-admin','js-field-group','js-settings') );
-// 
+//
 // gulp.task('pre-build', gulp.parallel('providers','leaflet-css') );
-// 
+//
 // gulp.task('build', gulp.parallel('pre-build','scss','js') );
-// 
-// 
+//
+//
 // gulp.task('watch', function() {
 // 	// place code for your default task here
 // 	gulp.watch('./src/scss/**/*.scss',	gulp.parallel( 'scss' ) );
 // 	gulp.watch('./src/js/**/*.js',		gulp.parallel( 'js' ) );
 // });
-// 
+//
 // gulp.task( 'dev', gulp.series('pre-build', 'build', 'watch') );
-// 
+//
 // gulp.task('default',cb => {
 // 	console.log('run either `gulp build` or `gulp dev`');
 // 	cb();
 // });
-// 
+//
 // module.exports = {
 // 	build:gulp.series('build')
 // }
