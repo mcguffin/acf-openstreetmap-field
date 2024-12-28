258faaa3e3b0ddccdb8e4c9d991eed2587dfac93
mcguffin <joern@flyingletters.com>
2024-12-28 12:57:36 +0100

    Fix: geocoder result overflow

--- src/js/lib/leaflet/corners.js
+++ src/js/lib/leaflet/corners.js
@@ -6,10 +6,18 @@ const addCorners = map => {
 	const above = L.DomUtil.create('div', 'acf-osm-above leaflet-above' )
 	const below = L.DomUtil.create('div', 'acf-osm-below leaflet-below' )
 
+
+	map.on('resize', () => {
+		const mapHeight = map._container.getBoundingClientRect().height
+		above.style.setProperty( '--map-height', mapHeight )
+		below.style.setProperty( '--map-height', mapHeight )
+	})
+
 	map._container.before( above )
 	map._container.after( below )
 	map._controlCorners['above'] = above
 	map._controlCorners['below'] = below
+
 };
 
 
