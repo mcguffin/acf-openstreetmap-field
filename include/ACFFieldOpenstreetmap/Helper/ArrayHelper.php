c0bf7d59778300f0ce40a5440d92df373fa6423e
mcguffin <joern@flyingletters.com>
2024-12-29 15:08:18 +0100

    fix ArrayHelper::filter_recursive

--- include/ACFFieldOpenstreetmap/Helper/ArrayHelper.php
+++ include/ACFFieldOpenstreetmap/Helper/ArrayHelper.php
@@ -4,13 +4,21 @@ namespace ACFFieldOpenstreetmap\Helper;
 
 class ArrayHelper {
 
+	/**
+	 *	@param array $array
+	 *	@param callable $callback
+	 *	@return array
+	 */
 	public static function filter_recursive( $array, $callback = 'boolval' ) {
-		return array_filter( $array, function($value) use ( $callback ) {
-			if ( is_array( $value ) ) {
-				return self::filter_recursive( $value, $callback );
+		$array = array_map( function( $value ) use ( $callback ) {
+			if ( $callback($value) ) {
+				if ( is_array( $value ) ) {
+					return self::filter_recursive( $value, $callback );
+				}
+				return $value;
 			}
-			return $callback($value);
-		});
-
+			return false;
+		}, $array );
+		return array_filter( $array, $callback );
 	}
 }
